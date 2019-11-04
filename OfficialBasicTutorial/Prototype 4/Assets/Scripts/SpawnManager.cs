fa8d3aa88df1df745576877d2cf5b47f040c66e0
lsw5955 <lsw5955@qq.com>
2019-11-05 07:36:45 +0800

    https://learn.unity.com/tutorial/lesson-plan-4-2-follow-the-player?language=en&projectId=5cf96846edbc2a2bcde6d0fc#5ce377e5edbc2a2e52df60d8

---
+++ OfficialBasicTutorial/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPfb;
+    private float spawnRange = 9;
+    float spawnPosX;
+    float spawnPosZ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(enemyPfb, GenerateSpawnPosition(), enemyPfb.transform.rotation);
+    }
+
+
+    Vector3 GenerateSpawnPosition()
+    {
+        spawnPosX = Random.Range(-spawnRange, spawnRange);
+        spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        return new Vector3(spawnPosX,  0, spawnPosZ);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
