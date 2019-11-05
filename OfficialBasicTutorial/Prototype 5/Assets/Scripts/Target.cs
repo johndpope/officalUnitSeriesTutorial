618acf195bfe5da0bca06ee0d5aef15b7c8eb474
铁拳无敌俞大猷 <lishiwen@qq.com>
2019-11-05 17:23:02 +0800

    https://learn.unity.com/tutorial/lesson-5-2-keeping-score?projectId=5cf96bdeedbc2a2b475972b3#

---
+++ OfficialBasicTutorial/Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private GameManager gameManager;
+    //Score which get by destroying gameObject when MouseDown
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    private Rigidbody rb;
+    float maxSpeed = 12;
+    float minSpeed = 16;
+
+    float maxTorque = 10;
+
+    float xRange = 4;
+    float ySpawnPos = -6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(RandomForce(), ForceMode.Impulse);
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange,xRange),ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
